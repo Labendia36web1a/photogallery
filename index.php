942f86e396b369d35ce71ea330a452cac5fccba0
Allenlabendia.usjrweb1a36 <53499680+Labendia36web1a@users.noreply.github.com>
2019-08-15 10:26:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+  <title>Log in</title>
+  </head>
+  <body>
+    <h1 align="Center">Allen Images</h1>
+    <main>
+      <h2 align="center">Log in</h2>
+      <form action="gallery.php>
+      <div>
+        <label for="uname" align="center"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+                                                                    </div>
+      <div>                                                               
+        <label for="psw" align="center"><b>Password</b></label>
+        <input type="text" align="center" placeholder="Enter Password" name="psw" required>
+        
+        <button type="submit" align="align">Log in</button>
+      </div>
+      
+      <div>
+   
+      <span class="psw"><a href="forgotpassword.php">Forgot Password?</a></span>
+      </div>
+      </form>
+      
+      </body>
+      </html>
