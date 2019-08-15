22290d8e32f36e508bfee2418a5baa791416cf2b
Allenlabendia.usjrweb1a36 <53499680+Labendia36web1a@users.noreply.github.com>
2019-08-15 10:24:31 +0800

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Assignment</title>
+  </head>
+<body>
+  <h1>Rivas Photo Gallery</h1>
+  <h2>List of photos</h2>
+  <nav>
+    <ul>
+      <li><a href="photo1.php">Photo 1</a></li>
+      <li><a href="photo2.php">Photo 2</a></li>
+      <li><a href="photo3.php">Photo 3</a></li>
+    </ul>
+  </nav>
+  </body>
+  </html>
